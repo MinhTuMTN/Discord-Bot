b1bf5446daedb7cf0e8446675c63844c9ae29d0a
MinhTuMTN <88047847+MinhTuMTN@users.noreply.github.com>
2023-08-08 08:07:36 +0000

    Chat Bard with context

--- main.py
+++ main.py
@@ -10,34 +10,40 @@ client = discord.Client(intents=discord.Intents.all())
 
 token_1 = os.getenv('bard_cookie_1')
 token_2 = os.getenv('bard_cookie_2')
+chatbot = None
 
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+  print(f'We have logged in as {client.user}')
 
 
 @client.event
 async def on_message(message: discord.Message):
-    if message.author == client.user:
-        return
+  global chatbot
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello, I am your Discord bot!')
-    elif message.content.startswith('!thptqg'):
-        identificationNumber = message.content.split(' ')[1]
-        scores = thptqg.scoreLookup(identificationNumber)
+  if message.author == client.user:
+    return
 
-        text = 'Số báo danh không hợp lệ hoặc không thể tìm thấy kết quả'
-        if (scores != None):
-            text = scores
+  if message.content.startswith('!hello'):
+    await message.channel.send('Hello, I am your Discord bot!')
+  elif message.content.startswith('!thptqg'):
+    identificationNumber = message.content.split(' ')[1]
+    scores = thptqg.scoreLookup(identificationNumber)
 
-        await message.reply(content=text)
-    elif message.content.startswith('!bard'):
-        chatbot = await AsyncChatbot.create(token_1, token_2)
-        question = ' '.join(message.content.split(' ')[1:])
-        response = await chatbot.ask(question)
+    text = 'Số báo danh không hợp lệ hoặc không thể tìm thấy kết quả'
+    if (scores != None):
+      text = scores
 
-        await message.reply(content=response['content'])
+    await message.reply(content=text)
+  elif message.content.startswith('!bard'):
+    if chatbot == None:
+      chatbot = await AsyncChatbot.create(token_1, token_2)
+
+    question = ' '.join(message.content.split(' ')[1:])
+    response = await chatbot.ask(question)
+
+    await message.reply(content=response['content'])
+
 
 client.run(TOKEN)
